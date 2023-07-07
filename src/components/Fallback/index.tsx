73c48f7c37f5e86a98f7b137c1a404fad5e879df
Dmitriy Vasilev <raoffonom@icloud.com>
2023-07-07 15:01:31 +0700

    fix styles

--- src/components/Fallback/index.tsx
+++ src/components/Fallback/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Image, StyleSheet } from 'react-native'
+import { Image } from 'react-native'
 import { s } from 'react-native-size-matters'
 
 import { CenterView } from '../CenterView'
@@ -8,14 +8,13 @@ import { CenterView } from '../CenterView'
 export function Fallback() {
   return (
     <CenterView>
-      <Image source={require('../../../assets/icons/1080.png')} style={img} />
+      <Image
+        source={require('../../../assets/icons/1080.png')}
+        style={{
+          width: s(160),
+          height: s(160),
+        }}
+      />
     </CenterView>
   )
 }
-
-const { img } = StyleSheet.create({
-  img: {
-    width: s(160),
-    height: s(160),
-  },
-})
--- src/components/ImageSwiper/index.tsx
+++ src/components/ImageSwiper/index.tsx
@@ -3,141 +3,18 @@ import React, { useRef } from 'react'
 import { nanoid } from 'nanoid/non-secure'
 import {
   Animated,
-  FlatList,
   ImageBackground,
   Pressable,
   StyleSheet,
   View,
   useColorScheme,
 } from 'react-native'
-import { s, vs } from 'react-native-size-matters'
+import { vs } from 'react-native-size-matters'
 
 import { W, black, white } from '../../constants'
-import { Text } from '../TextComponents'
-
-interface renderItemsI {
-  item: string
-  index: number
-}
-
-function RenderSwiperItems({ item, index }: renderItemsI) {
-  return (
-    <ImageBackground source={{ uri: item }} style={imageBg}>
-      {/* ... */}
-      {/* <Text oneColor="red" title={`${index}`} h="h0" /> */}
-    </ImageBackground>
-  )
-}
-
-interface renderDotsI {
-  index: number
-}
-
-interface SwiperI {
-  images: string[]
-  height: number
-}
 
 const dotWidth = vs(10)
 const dotMargin = vs(5)
-const fullDotWidth = dotWidth + dotMargin * 2
-
-export function ImageSwiper({ images, height }: SwiperI) {
-  const swiperRef = useRef<any>()
-  const AnimDot = Animated.createAnimatedComponent(Pressable)
-  const scrollX = useRef(new Animated.Value(0)).current
-
-  const dotsCountInView = Math.floor(W / fullDotWidth)
-
-  function RenderDots({ index }: renderDotsI) {
-    const inputRange = [W * (index - 1), W * index, W * (index + 1)]
-    const scale = scrollX.interpolate({
-      inputRange,
-      outputRange: [1, 1.3, 1],
-      extrapolate: 'clamp',
-    })
-    const opacity = scrollX.interpolate({
-      inputRange,
-      outputRange: [0.6, 1, 0.6],
-      extrapolate: 'clamp',
-    })
-    const dotColor = useColorScheme() === 'dark' ? white : black
-    return (
-      <AnimDot
-        onPressIn={() => swiperRef.current.scrollTo({ x: index * W, animated: true })}
-        key={index.toString()}
-        style={[
-          dot,
-          {
-            transform: [
-              {
-                scale,
-              },
-            ],
-            opacity,
-            backgroundColor: dotColor,
-          },
-        ]}
-      />
-    )
-  }
-
-  return (
-    <View style={[container, { height }]}>
-      <Animated.ScrollView
-        disableIntervalMomentum
-        decelerationRate={0.8}
-        ref={swiperRef}
-        removeClippedSubviews={false}
-        showsHorizontalScrollIndicator={false}
-        horizontal
-        snapToInterval={W}
-        style={swiperContainer}
-        onScroll={Animated.event(
-          [
-            {
-              nativeEvent: {
-                contentOffset: {
-                  x: scrollX,
-                },
-              },
-            },
-          ],
-          { useNativeDriver: true },
-        )}
-      >
-        {images.map((item, index) => (
-          <RenderSwiperItems key={nanoid()} index={index} item={item} />
-        ))}
-      </Animated.ScrollView>
-      {images.length > 0 && (
-        <Animated.View
-          style={[
-            dotContainer,
-            {
-              transform: [
-                {
-                  translateX: scrollX.interpolate({
-                    inputRange: [
-                      W * (dotsCountInView / 2),
-                      W * images.length + W * (dotsCountInView / 2),
-                    ],
-                    outputRange: [0, (W * -1 * images.length) / dotsCountInView],
-                    extrapolate: 'clamp',
-                  }),
-                },
-              ],
-            },
-          ]}
-        >
-          {images.map((a, id) => (
-            <RenderDots key={id.toString()} index={id} />
-          ))}
-        </Animated.View>
-      )}
-    </View>
-  )
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -167,4 +44,125 @@ const styles = StyleSheet.create({
   },
 })
 
-const { swiperContainer, imageBg, dotContainer, dot, container } = styles
+interface renderItemsI {
+  item: string
+  index: number
+}
+
+function RenderSwiperItems({ item }: renderItemsI) {
+  return (
+    <ImageBackground source={{ uri: item }} style={styles.imageBg}>
+      {/* ... */}
+      {/* <Text oneColor="red" title={`${index}`} h="h0" /> */}
+    </ImageBackground>
+  )
+}
+
+interface renderDotsI {
+  index: number
+}
+
+interface SwiperI {
+  images: string[]
+  height: number
+}
+
+const fullDotWidth = dotWidth + dotMargin * 2
+
+export function ImageSwiper({ images, height }: SwiperI) {
+  const swiperRef = useRef<any>()
+  const AnimDot = Animated.createAnimatedComponent(Pressable)
+  const scrollX = useRef(new Animated.Value(0)).current
+
+  const dotsCountInView = Math.floor(W / fullDotWidth)
+
+  function RenderDots({ index }: renderDotsI) {
+    const inputRange = [W * (index - 1), W * index, W * (index + 1)]
+    const scale = scrollX.interpolate({
+      inputRange,
+      outputRange: [1, 1.3, 1],
+      extrapolate: 'clamp',
+    })
+    const opacity = scrollX.interpolate({
+      inputRange,
+      outputRange: [0.6, 1, 0.6],
+      extrapolate: 'clamp',
+    })
+    const dotColor = useColorScheme() === 'dark' ? white : black
+
+    return (
+      <AnimDot
+        onPressIn={() => swiperRef.current.scrollTo({ x: index * W, animated: true })}
+        key={index.toString()}
+        style={[
+          styles.dot,
+          {
+            transform: [
+              {
+                scale,
+              },
+            ],
+            opacity,
+            backgroundColor: dotColor,
+          },
+        ]}
+      />
+    )
+  }
+
+  return (
+    <View style={[styles.container, { height }]}>
+      <Animated.ScrollView
+        disableIntervalMomentum
+        decelerationRate={0.8}
+        ref={swiperRef}
+        removeClippedSubviews={false}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        snapToInterval={W}
+        style={styles.swiperContainer}
+        onScroll={Animated.event(
+          [
+            {
+              nativeEvent: {
+                contentOffset: {
+                  x: scrollX,
+                },
+              },
+            },
+          ],
+          { useNativeDriver: true },
+        )}
+      >
+        {images.map((item, index) => (
+          <RenderSwiperItems key={nanoid()} index={index} item={item} />
+        ))}
+      </Animated.ScrollView>
+      {images.length > 0 && (
+        <Animated.View
+          style={[
+            styles.dotContainer,
+            {
+              transform: [
+                {
+                  translateX: scrollX.interpolate({
+                    inputRange: [
+                      W * (dotsCountInView / 2),
+                      W * images.length + W * (dotsCountInView / 2),
+                    ],
+                    outputRange: [0, (W * -1 * images.length) / dotsCountInView],
+                    extrapolate: 'clamp',
+                  }),
+                },
+              ],
+            },
+          ]}
+        >
+          {images.map((a, id) => (
+            <RenderDots key={id.toString()} index={id} />
+          ))}
+        </Animated.View>
+      )}
+    </View>
+  )
+}
