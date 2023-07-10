c7d24a9ec2202a702dba3395843731cc807d8a50
Dmitriy Vasilev <raoffonom@icloud.com>
2023-07-10 20:16:24 +0700

    fix for translate

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,8 +3,16 @@ import { isoCountry } from 'iso-country'
 import { initReactI18next } from 'react-i18next'
 import * as RNLocalize from 'react-native-localize'
 
+import ar from './locales/ar/translation.json'
+import bn from './locales/bn/translation.json'
 import en from './locales/en/translation.json'
+import fr from './locales/fr/translation.json'
+import mr from './locales/mr/translation.json'
+import ms from './locales/ms/translation.json'
 import ru from './locales/ru/translation.json'
+import te from './locales/te/translation.json'
+import tr from './locales/tr/translation.json'
+import uk from './locales/uk/translation.json'
 
 const locales = RNLocalize.getLocales()
 
@@ -20,11 +28,35 @@ if (!lang) {
 export const flagEmoji = isoCountry(locales[0]?.countryCode)?.emoji ?? '🇷🇺'
 
 const resources = {
+  ar: {
+    translation: ar,
+  },
+  bn: {
+    translation: bn,
+  },
   en: {
     translation: en,
   },
+  fr: {
+    translation: fr,
+  },
+  mr: {
+    translation: mr,
+  },
+  ms: {
+    translation: ms,
+  },
   ru: {
     translation: ru,
+  },
+  te: {
+    translation: te,
+  },
+  tr: {
+    translation: tr,
+  },
+  uk: {
+    translation: uk,
   },
 }
 
@@ -34,7 +66,7 @@ for (const [key, value] of Object.entries(resources)) {
   }
 }
 
-export const supportedLngs = ['en', 'ru']
+export const supportedLngs = ['ar', 'bn', 'en', 'fr', 'mr', 'ms', 'ru', 'te', 'tr', 'uk']
 
 export const isSupportedLang = supportedLngs.includes(lang)
 export const ruOrEnLang = lang === 'ru' ? 'ru' : 'en'
