ac58d211ef8e0eb8dc9d23f1fe05f74b633a4952
Dmitriy Vasilev <raoffonom@icloud.com>
2023-07-31 16:21:52 +0700

    test

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,17 @@ module.exports = {
   preset: 'react-native',
   setupFiles: ['./jest.setup.js'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        babelConfig: true
+      }
+    ],
+    '^.+\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest',
+    '\\.(ts|tsx)$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|leela)/)',
-    'node_modules/(?!(@sentry/react-native)/)',
-    'node_modules/(?!(jest-)?react-native|@react-native-community|@react-navigation|@react-native)'
+    'node_modules/(?!(react-native|@react-native-community|@react-navigation|@react-native/polyfills)/)'
   ]
 }
